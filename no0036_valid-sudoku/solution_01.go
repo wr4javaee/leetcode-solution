fa9f1e585ecc1953cce8767a8a865303ca20b7d7
ranwang3 <ranwang3@yirendai.com>
2018-11-28 19:48:51 +0800

    update no36

--- no0036_valid-sudoku/solution_01.go
+++ no0036_valid-sudoku/solution_01.go
@@ -3,22 +3,28 @@ package no0036_valid_sudoku
 import (
 	"strconv"
 	"strings"
+
+	//"strings"
 )
 
 func isValidSudoku(board [][]byte) bool {
-	// row
-	// column
-	// grid
+	// row 转义为 rowNo + 'r' + val
+	// column 转义为 columnNo + 'r' + val
+	// grid 转义为 rowNo + columnNo + val
 	valMap := make(map[string]string, 89)
+	// 按顺序遍历每一个九宫格数字
 	for row, valArr := range board {
 		for column, valByte := range valArr {
 			valStr := convertByteToStr(valByte)
+			// 不考虑空格.
 			if strings.Compare(valStr, ".") == 0 {
 				continue
 			}
+			// 对数字进行转义
 			rowKey := convertRowKey(row, valStr)
 			columnKey := convertColumnKey(column, valStr)
 			gridKey := convertGridKey(row, column, valStr)
+			// 判断数字是否重复, 重复则为不合法九宫格
 			if _, ok := valMap[rowKey]; ok {
 				return false
 			}
@@ -28,6 +34,7 @@ func isValidSudoku(board [][]byte) bool {
 			if _, ok := valMap[gridKey]; ok {
 				return false
 			}
+			// 将转义后数字存至map
 			valMap[rowKey] = rowKey
 			valMap[columnKey] = columnKey
 			valMap[gridKey] = gridKey
@@ -36,23 +43,27 @@ func isValidSudoku(board [][]byte) bool {
 	return true
 }
 
+// byte to string
 func convertByteToStr(b byte) string {
 	return string(b)
 }
 
+// 转义行数字
 func convertRowKey(rowNo int, val string) (rowKey string) {
 	rowKey = strconv.Itoa(rowNo) + "r" + val
 	return
 }
 
+// 转义列数字
 func convertColumnKey(columnNo int, val string) (columnKey string) {
 	columnKey = strconv.Itoa(columnNo) + "c" + val
 	return
 }
 
+// 转义小九宫格数字
 func convertGridKey(rowNo int, columnNo int, val string) (gridKey string) {
 	rowNo = rowNo / 3
 	columnNo = columnNo / 3
-	gridKey = strconv.Itoa(rowNo) + strconv.Itoa(columnNo) + "g" + val
+	gridKey = strconv.Itoa(rowNo) + strconv.Itoa(columnNo) + val
 	return
 }
--- no0036_valid-sudoku/solution_01_test.go
+++ no0036_valid-sudoku/solution_01_test.go
@@ -2,21 +2,26 @@ package no0036_valid_sudoku
 
 import "testing"
 
+// 定义单元测试数据类型
 type testCaseModel struct {
 	requestModel
 	responseModel
 }
 
+// 定义被测试方法请求参数数据类型
 type requestModel struct {
 	board [][]byte
 }
 
+// 定义被测试方法返回参数数据类型
 type responseModel struct {
 	result bool
 }
 
+// 测试方法
 func TestIsValidSudoku(t *testing.T) {
 	testCaseArr := []testCaseModel {
+		// 合法的九宫格
 		{
 			requestModel {
 				[][]byte {
@@ -31,9 +36,62 @@ func TestIsValidSudoku(t *testing.T) {
 					[]byte("....8..79"),
 				}},
 			responseModel {true},
+
+		},
+		// 行存在重复数据
+		{
+			requestModel {
+				[][]byte {
+					[]byte("53..5...."),
+					[]byte("6..195..."),
+					[]byte(".98....6."),
+					[]byte("8...6...3"),
+					[]byte("4..8.3..1"),
+					[]byte("7...2...6"),
+					[]byte(".6....28."),
+					[]byte("...419..5"),
+					[]byte("....8..79"),
+				}},
+			responseModel {false},
+
+		},
+		// 列存在重复数据
+		{
+			requestModel {
+				[][]byte {
+					[]byte("53..7...."),
+					[]byte("6..195..."),
+					[]byte(".98....6."),
+					[]byte("8...6...3"),
+					[]byte("4..8.3..1"),
+					[]byte("5...2...6"),
+					[]byte(".6....28."),
+					[]byte("...419..5"),
+					[]byte("....8..79"),
+				}},
+			responseModel {false},
+
+		},
+		// 小九宫格存在重复数据
+		{
+			requestModel {
+				[][]byte {
+					[]byte("53..7...."),
+					[]byte("6..195..."),
+					[]byte(".98....6."),
+					[]byte("8...6...3"),
+					[]byte("4..8.3..1"),
+					[]byte("7...2...6"),
+					[]byte("96....28."),
+					[]byte("..9417..5"),
+					[]byte("....8..79"),
+				}},
+			responseModel {false},
+
 		},
 	}
 
+	// 执行测试方法
 	for _, v := range testCaseArr {
 		req, res := v.requestModel, v.responseModel
 		if res.result == isValidSudoku(req.board) {
